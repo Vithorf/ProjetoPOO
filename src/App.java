0ef69af1cf24c89e5d326d445423a5fe94c57c85
Vithor Freitas <vithorfreitass@gmail.com>
2022-03-17 00:02:44 -0300

    coloquei um espaço

--- src/App.java
+++ src/App.java
@@ -13,7 +13,7 @@ import Estado.Estado;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! ");
         
     }
 }
