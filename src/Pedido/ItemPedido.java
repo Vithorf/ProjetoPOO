ab328688dbc5e08f030f30e55984fccd6cb212a4
Vithor Freitas <vithorfreitass@gmail.com>
2022-04-08 10:28:54 -0300

    CriaPedido e atualizaPedido

--- src/Pedido/ItemPedido.java
+++ src/Pedido/ItemPedido.java
@@ -12,10 +12,50 @@ public class ItemPedido {
 
     public ItemPedido(int quantidade, BigDecimal precoUnitario, BigDecimal precoTotal, String observacao,
             Produto produto) {
-        this.quantidade = quantidade;
-        this.precoUnitario = precoUnitario;
-        this.precoTotal = precoTotal;
-        this.observacao = observacao;
+        this.setQuantidade(quantidade);
+        this.setPrecoUnitario(precoUnitario);
+        this.setPrecoTotal(precoTotal);
+        this.setObservacao(observacao);
+        this.setProduto(produto);
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
         this.produto = produto;
+    }
+
+    public BigDecimal getPrecoTotal() {
+        return precoTotal;
+    }
+
+    public void setPrecoTotal(BigDecimal precoTotal) {
+        this.precoTotal = precoTotal;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 }
